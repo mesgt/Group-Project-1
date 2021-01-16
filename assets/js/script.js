48600c9e66bb1c22757bcd9e1030bbd176d0a977
Evan St Martin <evanstm011@gmail.com>
2021-01-16 15:28:43 -0600

    user search appers in appended list

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,19 @@ $(document).ready(function () {
   $("#submitFood").on("click", function (event) {
     event.preventDefault();
     var foodItem = $("#search").val();
-    console.log(foodItem);
+    //console.log(foodItem);
     userInput.push(foodItem);
     localStorage.setItem("userInput", JSON.stringify(userInput));
+    function renderButtons() {
+      $("#foodList").empty();
+      for (var i = 0; i < foodItem.length; i++) {
+          var addList = $("<li>");
+          addList.addClass("foodItem");
+          var foodButton= $("<button>");
+          foodButton.addClass("btn rounded");
+          foodButton.text(foodItem[i]);
+      }
+    }
     function foodStringRequest() {
       let queryFoodURL = `https://api.edamam.com/api/nutrition-data?app_id=c502f564&app_key=a522a1a262d4a5a3968b56ede64ba74a&ingr=${foodItem}`;
 
@@ -15,10 +25,13 @@ $(document).ready(function () {
         url: queryFoodURL,
         method: "GET",
       }).then(function (response) {
-        console.log(response);
+        //console.log(response);
+        $("#foodList").text(response.ingredients[0].text);
       });
     }
     foodStringRequest();
+    renderButtons();
   });
-  console.log(userInput);
+  
+  //console.log(userInput);
 });
