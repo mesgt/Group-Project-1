366510e7b0bf2afab8ded228ee65e4fdefd19789
Maria Sargent <maria.marshall.83@gmail.com>
2021-01-16 15:07:07 -0600

    Recipe name displaying on page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,40 @@
 
 $(document).ready(function () {
-  let foodStringRequest = "2 cups ice cream";
-  let queryFoodURL = `https://api.edamam.com/api/nutrition-data?app_id=c502f564&app_key=a522a1a262d4a5a3968b56ede64ba74a&ingr=${foodStringRequest}`;
+    let foodStringRequest = "2 cups ice cream";
+    let queryFoodURL = `https://api.edamam.com/api/nutrition-data?app_id=c502f564&app_key=a522a1a262d4a5a3968b56ede64ba74a&ingr=${foodStringRequest}`;
 
-  $.ajax({
-    url: queryFoodURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response.calories);
-  });
-
-
-    
-   
-
-
-
-
-
-
-    //event listener for submit button
-    $("#submitFood").click(function () {
-        alert("chicken!");
+    $.ajax({
+        url: queryFoodURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response.calories);
     });
 
-    //Second API- recipe
+
+    //Second API- recipe. Triggered when user enters new food ingredient.
     let recipeRequest = "rice";//this variable will need to be global- food user Input
     let queryRecipeURL = `https://yummly2.p.rapidapi.com/feeds/search?start=0&maxResult=18&FAT_KCALMax=1000&maxTotalTimeInSeconds=7200&allowedAttribute=diet-lacto-vegetarian%2Cdiet-low-fodmap&q=${recipeRequest}`;
-  
+
     $.ajax({
         "async": true,
         "crossDomain": true,
-      url: queryRecipeURL,
-      method: "GET",
-      "headers": {
-        "x-rapidapi-key": "d3e95b7309msh0734a6ab22cb20cp1b5f68jsn85dde42938a0",
-        "x-rapidapi-host": "yummly2.p.rapidapi.com"
-    }
+        url: queryRecipeURL,
+        method: "GET",
+        "headers": {
+            "x-rapidapi-key": "d3e95b7309msh0734a6ab22cb20cp1b5f68jsn85dde42938a0",
+            "x-rapidapi-host": "yummly2.p.rapidapi.com"
+        }
     }).then(function (response) {
-      console.log(response);
-      console.log(response.relatedPhrases.recipes[0]);
-    });
+        console.log(response);
+        console.log(response.relatedPhrases.recipes[0]);
+        var recipe = response.relatedPhrases.recipes[0];
+        $("#recipeName").text(JSON.stringify(recipe)); //Need text after <>, not the whole thing
+        // var mealImageURL = JSON.parse(response.parsedIngredients[0].image-url);
+        // $("#recipeImage").attr("src", mealImageURL);
+        
 
     });
+
+});
 
 
