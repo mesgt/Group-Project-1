ed06b63d55c8b29400227ca93636f2e759013dba
Evan St Martin <evanstm011@gmail.com>
2021-01-16 12:49:38 -0600

    added user search function, results in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,23 @@
-let foodStringRequest = "1lb chicken";
-
 $(document).ready(function () {
-    let queryFoodURL = `https://api.edamam.com/api/nutrition-data?app_id=c502f564&app_key=a522a1a262d4a5a3968b56ede64ba74a&ingr=${foodStringRequest}`;
+  //user searches through api data
+  var userInput = JSON.parse(localStorage.getItem(userInput)) || [];
+  $("#submitFood").on("click", function (event) {
+    event.preventDefault();
+    var foodItem = $("#search").val();
+    console.log(foodItem);
+    userInput.push(foodItem);
+    localStorage.setItem("userInput", JSON.stringify(userInput));
+    function foodStringRequest() {
+      let queryFoodURL = `https://api.edamam.com/api/nutrition-data?app_id=c502f564&app_key=a522a1a262d4a5a3968b56ede64ba74a&ingr=${foodItem}`;
 
-    $.ajax({
+      $.ajax({
         url: queryFoodURL,
         method: "GET",
-    }).then(function (response) {
-        console.log(response.calories);
-
-
-    });
-    
-    function userInput (params) {
-        
+      }).then(function (response) {
+        console.log(response);
+      });
     }
-
-
-
-
-
-
-
-    //event listener for submit button
-    $("#submitFood").click(function () {
-        alert("chicken!");
-    });
-
+    foodStringRequest();
+  });
+  console.log(userInput);
 });
