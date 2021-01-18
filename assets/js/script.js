8c518025af390db877cddeee27541868e39b395f
Maria Sargent <maria.marshall.83@gmail.com>
2021-01-18 00:38:39 -0600

    Added recipe instructions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function () {
-    let foodStringRequest = "2 cups ice cream";
+    let foodStringRequest = "2 cups ice cream";//this variable will need to be global- food user Input
     let queryFoodURL = `https://api.edamam.com/api/nutrition-data?app_id=c502f564&app_key=a522a1a262d4a5a3968b56ede64ba74a&ingr=${foodStringRequest}`;
 
     $.ajax({
@@ -13,32 +13,46 @@ $(document).ready(function () {
 
     //Second API- recipe. Triggered when user enters new food ingredient.
     
-    let recipeRequest = "cabbage";//this variable will need to be global- food user Input
+    let recipeRequest = "beef";//this variable will need to be global- food user Input
     let queryRecipeURL = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${recipeRequest}&number=5&limitLicense=${true}&ranking=1&ignorePantry=${true}&apiKey=4da9dd4148874160a27f2aee5c61d935`;
     //FIND RECIPE NAME
     $.ajax({
         url: queryRecipeURL,
         method: "GET",
 
-    }).then(function (recipeResponse) {
+    }).success(function (recipeResponse) {
         console.log(recipeResponse);
         console.log(recipeResponse[0].title);
         var recipeLookUp = recipeResponse[0].title;
-        let findRecipeURL = `https://api.spoonacular.com//recipes/extract?url=https://foodista.com/recipe/${recipeLookUp}&forceExtraction=true&analyze=false&apiKey=4da9dd4148874160a27f2aee5c61d935`;
-        $("#recipeName").text(JSON.stringify(recipeLookUp)); 
+        $("#recipeName").text(JSON.stringify(recipeLookUp));
+        var recipeID = recipeResponse[0].id; //ID TO LOOK UP RECIPE INSTRUCTIONS
+        let findRecipeURL = `https://api.spoonacular.com/recipes/${recipeID}/information?includeNutrition=${false}&apiKey=4da9dd4148874160a27f2aee5c61d935`;
+        
         //FIND RECIPE INSTRUCTIONS
         $.ajax({
             url: findRecipeURL,
             method: "GET",
-        }).then(function (recipeFind) {
-        console.log(recipeFind);
-        console.log(recipeFind.instructions);
-        var recipeDisplay = recipeFind.instructions;
+    
+        }).success(function (recipeInstructions) {
+        console.log(recipeInstructions);
+        console.log(recipeInstructions.summary);
+        var recipeDisplay = recipeInstructions.instructions;
+        console.log(recipeDisplay);
         $("#recipe").text(JSON.stringify(recipeDisplay));
-        var mealImageURL = recipeFind.image;
-        console.log(mealImageURL);
-        $("#recipeImage").attr("src", mealImageURL);
-        });
+
+
+        // $.ajax({
+        //     url: findRecipeURL,
+        //     method: "GET",
+        // }).then(function (recipeFind) {
+        // console.log(recipeFind);
+        // console.log(recipeFind.instructions);
+        // var recipeDisplay = recipeFind.instructions;
+        // $("#recipe").text(JSON.stringify(recipeDisplay));
+        // var mealImageURL = recipeFind.image;
+        // console.log(mealImageURL);
+        // $("#recipeImage").attr("src", mealImageURL);
+        // });
 
 
 
@@ -48,4 +62,4 @@ $(document).ready(function () {
 
 });
 
-
+});
